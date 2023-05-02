26099597860ecc2d63f01ce582eabff5795a450b
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-05-02 15:36:44 +0530

    #21 Course & User Model Routes Controller

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
+
+// All Routes Import
+import course from "./routes/courseRoutes.js";
+import user from "./routes/userRoutes.js";
+
+// All Routes prefix
+app.use("api/v1", course);
+app.use("api/v1", user);
 
 export default app;
