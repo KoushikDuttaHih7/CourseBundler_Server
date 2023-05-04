6c73e39cfd7b0a621119fe935aec401054321900
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-05-04 16:25:47 +0530

    #32 Course, Lecture Controller

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -5,7 +5,7 @@ import ErrorHandler from "../utils/errorHandler.js";
 import cloudinary from "cloudinary";
 
 export const getAllCourse = catchAsyncError(async (req, res, next) => {
-  const courses = await Course.find().select("-lectures").lean();
+  const courses = await Course.find().lean();
   res.status(200).json({
     success: true,
     courses,
@@ -54,22 +54,28 @@ export const getCourseLecture = catchAsyncError(async (req, res, next) => {
   });
 });
 
-export const addCourseLecture = catchAsyncError(async (req, res, next) => {
+// Max video size 100mb
+export const addLecture = catchAsyncError(async (req, res, next) => {
   const { id } = req.params;
   const { title, description } = req.body;
-  // const file = req.file;
 
   const course = await Course.findById(id);
   if (!course) return next(new ErrorHandler("No Course Found", 404));
 
   // Upload file here
+  const file = req.file;
+  const fileUri = getDataUri(file);
+
+  const myCloud = await cloudinary.v2.uploader.upload(fileUri.content, {
+    resource_type: "video",
+  });
 
   course.lectures.push({
     title,
     description,
     video: {
-      public_id: "url",
-      url: "url",
+      public_id: myCloud.public_id,
+      url: myCloud.secure_url,
     },
   });
 
@@ -80,5 +86,56 @@ export const addCourseLecture = catchAsyncError(async (req, res, next) => {
   res.status(200).json({
     success: true,
     message: "Lecture added in Course",
+  });
+});
+
+export const deleteCourse = catchAsyncError(async (req, res, next) => {
+  const { id } = req.params;
+
+  const course = await Course.findById(id);
+  if (!course) return next(new ErrorHandler("No Course Found", 404));
+
+  await cloudinary.v2.uploader.destroy(course.poster.public_id);
+
+  for (let i = 0; i < course.lectures.length; i++) {
+    const singleLecture = course.lectures[i];
+    await cloudinary.v2.uploader.destroy(singleLecture.video.public_id, {
+      resource_type: "video",
+    });
+  }
+
+  await course.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    message: "Course Deleted Successfully",
+  });
+});
+
+export const deleteLecture = catchAsyncError(async (req, res, next) => {
+  const { courseId, lectureId } = req.query;
+
+  const course = await Course.findById(courseId);
+  if (!course) return next(new ErrorHandler("No Course Found", 404));
+
+  const lecture = course.lectures.find((item) => {
+    if (item._id.toString() === lectureId.toString()) return item;
+  });
+
+  await cloudinary.v2.uploader.destroy(lecture.video.public_id, {
+    resource_type: "video",
+  });
+
+  course.lectures = course.lectures.filter((item) => {
+    if (item._id.toString() !== lectureId.toString()) return item;
+  });
+
+  course.numOfVideos = course.lectures.length;
+
+  await course.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Lecture Deleted Successfully",
   });
 });
--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,25 +1,38 @@
 import express from "express";
 import {
-  addCourseLecture,
+  addLecture,
   createCourse,
+  deleteCourse,
+  deleteLecture,
   getAllCourse,
   getCourseLecture,
 } from "../controllers/courseController.js";
 import singleUpload from "../middleware/multer.js";
+import { authorizeAdmin, isAuthenticated } from "../middleware/auth.js";
 const router = express.Router();
 
 // Get All Courses without Lectures
 router.route("/courses").get(getAllCourse);
 
 // Create new course - only Admin
-router.route("/createcourse").post(singleUpload, createCourse);
+router
+  .route("/createcourse")
+  .post(isAuthenticated, authorizeAdmin, singleUpload, createCourse);
 
 // Get Course Details
-router.route("/course/:id").get(getCourseLecture);
+router.route("/course/:id").get(isAuthenticated, getCourseLecture);
 
-// Add Lecture, Delete Course,
-router.route("/course/:id").post(singleUpload, addCourseLecture);
+// Add Lecture
+router
+  .route("/course/:id")
+  .post(isAuthenticated, authorizeAdmin, singleUpload, addLecture);
+
+// Delete Course
+router
+  .route("/course/:id")
+  .delete(isAuthenticated, authorizeAdmin, deleteCourse);
 
 // Delete Lecture
+router.route("/lecture").delete(isAuthenticated, authorizeAdmin, deleteLecture);
 
 export default router;
