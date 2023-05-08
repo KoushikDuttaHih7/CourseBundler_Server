4b991eb6d84e775b95e438c3a11a964533beaff3
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-05-08 15:14:39 +0530

    cors origin

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    orogin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
